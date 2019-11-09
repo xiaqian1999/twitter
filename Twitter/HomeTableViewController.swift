855c4e2147016d6ed1840826cbbd6efe512c5c51
xiaqian <xiaqian1999@gmail.com>
2019-11-08 20:47:27 -0500

    Like and Retweet features

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -100,7 +100,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileimageView.image = UIImage(data: imageData)
         }
         
-        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
